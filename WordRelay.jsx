43efc376ffbb86c8325f666f68dbf332fdc6ad56
redsky <hyun0238@gmail.com>
2020-10-25 20:16:59 +0900

    끝말잇기 class방식 코딩

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -39,6 +39,7 @@ class WordRelay extends React.Component {
       <>
         <div>{this.state.word}</div>
         <form onSubmit={this.onSubmitForm}>
+          // input에는 value와 onChange는 쌍으로 넣어줘야한다. 아니면 defaultValue를 셋팅 해줘야 한다.
           <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
           <button>입력!</button>
         </form>
